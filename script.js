719f74e7faed8e87da1b2aad3658e9ba90401174
salebzzz <sasa0030909@gmail.com>
2025-07-31 00:24:35 +0200

    Prvi commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+let name = "Sasa Osmanovic"; // string -> text
+
+let age = 42; // number -> integer
+
+let height = 174.5 // number / float / decimal
+
+let isProgramer = true // bool -> boolean (true/false) = true (1) / false (0)
+
+
+console.log(name, age, height, isProgramer);
+
+
+let ime = "Sasa"
+let prezime = "Osmanovic"
+
+let imeIPrezime = ime+" "+prezime;
+console.log(imeIPrezime);
+
+
+let prviBroj = 10;
+let drugiBroj = 20;
+
+let zbir = prviBroj+drugiBroj;
+console.log(zbir);
+
+
+
+let cena = 20;
+let kursEvra =  117.2;
+
+let cenaJogurta = 50;
+
+let dinari = cena*kursEvra;
+let jogurtDinari = cenaJogurta*kursEvra;
+console.log(dinari, jogurtDinari);
+
+let components = ["Racunar", "Tastatura", "Mis"];
+console.log(components);
